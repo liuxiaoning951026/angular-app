cb7d7b5c667646ab20b38bad6701281c908ed4b4
liuning <liuning11107@navinfo.com>
2024-08-28 13:29:22 +0800

    fix(router): 增加新路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MyNewComponentComponent } from './my-new-component/my-new-component.com
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { CenterComponent } from './center/center.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AboutComponent } from './about/about.component';
     MyNewComponentComponent,
     HomeComponent,
     AboutComponent,
+    CenterComponent,
   ],
   imports: [
     BrowserModule,
